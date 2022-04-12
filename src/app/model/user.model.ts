2bbde1cf1d8f8490b5781061daa8d0019d337feb
Stefan <stefan.joncic@united.group>
2022-04-12 12:30:09 +0200

    module

---
+++ src/app/model/user.model.ts
@@ -0,0 +1,11 @@
+import { Role } from "./role.model";
+
+export interface User {
+    id:number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    role: Role;
+    token?:string;
+}
